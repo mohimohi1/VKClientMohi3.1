f93e56759952b956261cff8c2e40f378edceb189
mohimohi1 <mo.misha@bk.ru>
2019-05-27 13:55:08 +0300

    1

---
+++ VKClienMohiMo/Cells/NewGroupCell.swift
@@ -0,0 +1,21 @@
+//
+//  NewGroupCell.swift
+//  VKClienMohiMo
+//
+//  Created by Михаил Мохнач on 27/05/2019.
+//  Copyright © 2019 Михаил Мохнач. All rights reserved.
+//
+
+import UIKit
+
+class NewGroupCell: UITableViewCell {
+    
+    
+    static let reuseidentifier = "NewGroupCell"
+    
+    
+    @IBOutlet weak var newgroupNameLabel: UILabel!
+    
+    @IBOutlet weak var newavagroupImageView: UIImageView!
+
+}
