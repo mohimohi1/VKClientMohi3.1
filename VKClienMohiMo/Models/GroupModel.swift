43c67861b1180774032186b9bb9de8bf3c75f16b
mohimohi1 <mo.misha@bk.ru>
2019-05-27 14:04:04 +0300

    1

---
+++ VKClienMohiMo/Models/GroupModel.swift
@@ -0,0 +1,22 @@
+//
+//  GroupModel.swift
+//  VKClienMohiMo
+//
+//  Created by Михаил Мохнач on 22/05/2019.
+//  Copyright © 2019 Михаил Мохнач. All rights reserved.
+//
+
+import UIKit
+
+struct GroupModel {
+    
+    var name:String
+    
+    var photo: UIImage
+}
+
+
+struct Photomodel {
+    
+    var photo: UIImage
+}
