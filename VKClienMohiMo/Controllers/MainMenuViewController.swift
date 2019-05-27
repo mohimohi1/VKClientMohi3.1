fbeaff9ac2fbf893c2c729ae0e81c0dec554f594
mohimohi1 <mo.misha@bk.ru>
2019-05-27 13:55:32 +0300

    1

---
+++ VKClienMohiMo/Controllers/MainMenuViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainMenuViewController.swift
+//  VKClienMohiMo
+//
+//  Created by Михаил Мохнач on 19/05/2019.
+//  Copyright © 2019 Михаил Мохнач. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func myUnwindAction1(unWindSegue:UIStoryboardSegue) {
+        
+    }
+
+}
